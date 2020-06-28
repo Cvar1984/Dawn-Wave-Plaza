94a471ccd32da7d5bc22e55fcfd63a689950042b
Cvar1984 <gedzsarjuncomuniti@gmail.com>
2020-06-28 21:48:16 +0800

    Middleware

--- config/middleware.php
+++ config/middleware.php
@@ -4,11 +4,7 @@ use Slim\App;
 use Slim\Middleware\ErrorMiddleware;
 use Slim\Views\TwigMiddleware;
 use Slim\Views\Twig;
-use Slim\Psr7\Response;
-use Slim\Exception\HttpNotFoundException;
 use Psr\Container\ContainerInterface as Container;
-use Psr\Http\Message\ServerRequestInterface as Request;
-use Psr\Http\Server\RequestHandlerInterface as Handler;
 
 return function (App $app, Container $container):void {
     // optional sub directory
@@ -17,32 +13,9 @@ return function (App $app, Container $container):void {
     // Add the Slim built-in routing middleware
     $app->addRoutingMiddleware();
     // Not found handler
-    $app->add(
-        function (Request $request, Handler $handler) {
-            try {
-                return $handler->handle($request);
-            } catch (HttpNotFoundException  $e) {
-                $response = (new Response())->withStatus(404);
-                $view = \Slim\Views\Twig::fromRequest($request);
-                $view->render(
-                    $response,
-                    'home.html.twig',
-                    [
-                        'title' => 'Page can\'t be found',
-                        'button' => 'Fuck go back',
-                        'not_found' => true,
-                        'links' => [
-                            'home' => '/home',
-                            'profile' => '/profile',
-                            'about' => '/about',
-                            'blog' => '/blog',
-                        ]
-                    ]
-                );
-                return $response;
-            }
-        }
-    );
+    $app->add(\App\Middleware\NotFoundMiddleware::class);
+    // database handler
+    $app->add(\App\Middleware\DatabaseMiddleware::class);
     // Catch exceptions and errors
     $app->add(ErrorMiddleware::class);
     // Add Twig-View Middleware
